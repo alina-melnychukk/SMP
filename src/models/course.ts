28e53f6d7506aba1bc5f10f08eb9e86978282b78
alina-melnychukk <alinameln2@gmail.com>
2024-10-06 22:43:20 +0300

    fix programm

--- src/models/course.ts
+++
@@ -1,7 +0,0 @@
-import { CourseType } from "./types";
-
-export type Course = {
-    id: number;
-    name: string;
-    type: CourseType;
-};
--- src/models/lesson.ts
+++
@@ -1,9 +0,0 @@
-import { DayOfWeek, TimeSlot } from "./types";
-
-export type Lesson = {
-    courseId: number;
-    professorId: number;
-    classroomNumber: string;
-    dayOfWeek: DayOfWeek;
-    timeSlot: TimeSlot;
-};
